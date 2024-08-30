e2c95dab74aaf28ce7a11a847b33399d4f33e36c
MattNewbigging <matt.newbigging@aize.io>
2024-08-30 10:37:26 +0100

    fidding with font

--- src/ui/game-screen/game-screen.tsx
+++ src/ui/game-screen/game-screen.tsx
@@ -3,5 +3,14 @@ import "../../app/app.scss";
 import React from "react";
 
 export const GameScreen: React.FC = () => {
-  return <div className="game-screen">Lock level</div>;
+  return (
+    <div className="game-screen">
+      <p className="align-left">Lockpick Skill</p>
+      <p>100</p>
+      <p className="align-left">Bobby Pins</p>
+      <p>90</p>
+      <p className="align-left">Lock Level</p>
+      <p>Average</p>
+    </div>
+  );
 };
