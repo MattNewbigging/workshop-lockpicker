08baef76c7425facab7c14ce38458c075eb72826
MattNewbigging <matt.newbigging@aize.io>
2024-09-03 14:02:15 +0100

    ui prompt

--- src/ui/game-screen/game-screen.tsx
+++ src/ui/game-screen/game-screen.tsx
@@ -5,12 +5,18 @@ import React from "react";
 export const GameScreen: React.FC = () => {
   return (
     <div className="game-screen">
-      <p className="align-left">Lockpick Skill</p>
-      <p>100</p>
-      <p className="align-left">Bobby Pins</p>
-      <p>90</p>
-      <p className="align-left">Lock Level</p>
-      <p>Average</p>
+      <div className="bottom-left">
+        <p className="align-left">Lockpick Skill</p>
+        <p>100</p>
+        <p className="align-left">Bobby Pins</p>
+        <p>90</p>
+        <p className="align-left">Lock Level</p>
+        <p>Average</p>
+      </div>
+
+      <div className="bottom-right">
+        <p>Show debug UI D)</p>
+      </div>
     </div>
   );
 };
